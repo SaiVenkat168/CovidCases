24fccd344a813552c64c33af6c45c5725671e7e7
SaiVenkat168 <saivenkatsalpala@gmail.com>
2023-11-25 00:59:23 +0530

    'commit'

--- src/components/covid.js
+++ src/components/covid.js
@@ -1,14 +1,18 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import './covid.css'
 
-const App=() =>{
+const Covid=() =>{
+
+    const [data,setData] = useState([]);
+
 
     const getCovidData=async()=>{
 
         try{
             const res = await fetch('https://data.covid19india.org/data.json');
             const actualdata = await res.json();
-
+            console.log(actualdata.statewise[0]);
+            setData(actualdata.statewise[0]);
         }
         catch(err)
         {
@@ -18,34 +22,21 @@ const App=() =>{
     }
 
 useEffect(()=>{
-   // getCovidData();
+    getCovidData();
 
 },[]);
 
   return(
     <>
-    <h1>🔴 Live</h1>
-    <h2>Covid Cases</h2>
-
-    
-     <ul>
-        <select>
-            <li className='card'>
-                    <div className='card_main'>
-                        <div className='card_inner'>
-                            <p className='card_name'><span>OUR</span> COUNTRY</p>
-                            <p className='card_total_card_small'>INDIA</p>
-                    
-                        </div>
-                    </div>
-                </li>
-                
+    <section>
+        <h1>🔴 Live</h1>
+        <h2>Covid Cases</h2>
+        <ul>
                 <li className='card'>
                     <div className='card_main'>
                         <div className='card_inner'>
                             <p className='card_name'><span>OUR</span> COUNTRY</p>
                             <p className='card_total_card_small'>INDIA</p>
-                    
                         </div>
                     </div>
                 </li>
@@ -53,9 +44,16 @@ useEffect(()=>{
                 <li className='card'>
                     <div className='card_main'>
                         <div className='card_inner'>
-                            <p className='card_name'><span>OUR</span> COUNTRY</p>
-                            <p className='card_total_card_small'>INDIA</p>
-                    
+                            <p className='card_name'><span>TOTAL</span> RECOVERED</p>
+                            <p className='card_total_card_small'>{data.recovered}</p>
+                        </div>
+                    </div>
+                </li>
+                <li className='card'>
+                    <div className='card_main'>
+                        <div className='card_inner'>
+                            <p className='card_name'><span>TOTAL</span> CONFIRMED</p>
+                            <p className='card_total_card_small'>{data.confirmed}</p>
                         </div>
                     </div>
                 </li>
@@ -63,9 +61,8 @@ useEffect(()=>{
                 <li className='card'>
                     <div className='card_main'>
                         <div className='card_inner'>
-                            <p className='card_name'><span>OUR</span> COUNTRY</p>
-                            <p className='card_total_card_small'>INDIA</p>
-                    
+                            <p className='card_name'><span>TOTAL</span> DEATHS</p>
+                            <p className='card_total_card_small'>{data.deaths}</p>
                         </div>
                     </div>
                 </li>
@@ -73,9 +70,8 @@ useEffect(()=>{
                 <li className='card'>
                     <div className='card_main'>
                         <div className='card_inner'>
-                            <p className='card_name'><span>OUR</span> COUNTRY</p>
-                            <p className='card_total_card_small'>INDIA</p>
-                    
+                            <p className='card_name'><span>TOTAL</span> ACTIVE</p>
+                            <p className='card_total_card_small'>{data.active}</p>
                         </div>
                     </div>
                 </li>
@@ -83,17 +79,14 @@ useEffect(()=>{
                 <li className='card'>
                     <div className='card_main'>
                         <div className='card_inner'>
-                            <p className='card_name'><span>OUR</span> COUNTRY</p>
-                            <p className='card_total_card_small'>INDIA</p>
-                    
+                            <p className='card_name'><span>LAST</span> UPDATED</p>
+                            <p className='card_total_card_small'>{data.lastupdatedtime}</p>
                         </div>
                     </div>
                 </li>
-
-        </select>
-        
-     </ul>
-
+            
+        </ul>
+    </section>
     </>
   )
 }
